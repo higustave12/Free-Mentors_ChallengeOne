1a7e8beb2a5ca6694503512a8dbd57140642e3e1
higustave12 <higustave123@gmail.com>
2019-08-30 08:17:53 +0200

    add login_user endpoint [Finishes #168205894]

--- FREE-MENTORS_BACKEND/ROUTES/Routes.js
+++ FREE-MENTORS_BACKEND/ROUTES/Routes.js
@@ -5,5 +5,5 @@ const router= express.Router();
 
 //User accounts routes
 router.post('/api/v1/auth/signup',account_controler.createUseraccount); //Endpoint to create user account
-
+router.post('/api/v1/auth/signin',account_controler.userLogin); //Endpoint to login a user
 export default router;
