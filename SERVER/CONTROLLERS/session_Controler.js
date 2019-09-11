cd0ce3859b93372e77b190bfeb51220d2e84007b
higustave12 <higustave123@gmail.com>
2019-09-11 23:19:33 +0200

    Remove database logic from mentor reject session endpoint [Improves #168424884]

--- SERVER/CONTROLLERS/session_Controler.js
+++ SERVER/CONTROLLERS/session_Controler.js
@@ -2,6 +2,7 @@ import pool from '../test/MODELS/create';
 import session_schema from '../JOI_VALIDATION/session_validation';
 //import session_review_schema from '../JOI_VALIDATION/session_review';
 import Joi from '@hapi/joi';
+import mentorRejectSession from '../SERVICES/rejectSessionQueries';
 
 class sessionControler{
     createSession(req, res){
@@ -79,45 +80,31 @@ class sessionControler{
     }
 
     
-    rejectSessionRequest(req, res) {
+    async rejectSessionRequest(req, res) {
         const sessioId = parseInt(req.params.sessionId);
-        pool.connect((err, client, done) => {
-            const select_query = `SELECT * FROM sessions WHERE sessionid= $1`;
-            const select_query_value = [sessioId];
-            client.query(select_query, select_query_value, (error, result) => {
-                if (result.rows[0]) {
-                    const slct_mentorId = req.user_token.id;
-                    const check_sessions_mentor = result.rows[0].mentorid;
-                    if (check_sessions_mentor == slct_mentorId) {
-                        const new_session_status = "rejected";
-                        const update_session_status_reject = 'UPDATE sessions SET status=$1 WHERE mentorid=$2 AND sessionid=$3';
-                        const update_session_status_reject_values = [new_session_status, slct_mentorId, sessioId];
-                        client.query(update_session_status_reject, update_session_status_reject_values, () => {
-                            const sel_reject_query = 'SELECT * FROM sessions WHERE sessionid=$1';
-                            const sel_reject_query_values = [sessioId];
-                            client.query(sel_reject_query, sel_reject_query_values, (error, result) => {
-                                const {sessionid, mentorid, menteeid, questions, menteeemail, status}= result.rows[0];
-                                return res.status(200).json({
-                                    status: 200,
-                                    data: {sessionId: sessionid, mentorId: mentorid, menteeId: menteeid, questions, menteeEmail: menteeemail, status}
-                                });
-                            });
-                        });
-                    } else {
-                        return res.status(404).json({
-                            status: 404,
-                            error: "You are not a mentor for this session"
-                        })
-                    }
-                } else {
-                    return res.status(404).json({
-                        status: 404,
-                        error: "No session with such Id found"
-                    })
-                }
-            });
-            done();
-        });
+        const sessionRejectResp= await mentorRejectSession.mentorRejectSelectFn(sessioId);
+        if(!sessionRejectResp){
+            return res.status(404).json({
+                status: 404,
+                error: "No session with such Id found"
+            })
+        }else{
+            const mentorId= req.user_token.id;
+            const checkSessionRejectMentorId= sessionRejectResp.mentorid; 
+            if(checkSessionRejectMentorId===mentorId){
+                const updateSessionRejectResp= await mentorRejectSession.updateSessionRejectFn(req, sessioId);
+                const {sessionid, mentorid,menteeid,questions,menteeemail,status}= updateSessionRejectResp;
+                return res.status(200).json({
+                    status: 200,
+                    data: {sessionId:sessionid, mentorId:mentorid, menteeId:menteeid, questions, menteeEmail:menteeemail, status}
+                });
+            }else{
+                return res.status(404).json({
+                    status: 404,
+                    error: "You are not a mentor for this session"
+                })
+            }
+        }
     }
 
     //Get or View all sessions
